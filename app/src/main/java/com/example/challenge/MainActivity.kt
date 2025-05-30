f38389eee864ce5afba5de07f896d9046c684fa1
Jemal Samba <a46123@alunos.ipb.pt>
2025-05-30 19:57:50 +0100

    changed the app name

--- app/src/main/java/com/example/challenge/MainActivity.kt
+++ app/src/main/java/com/example/challenge/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BookApp(navController = navController)
+                    App(navController = navController)
                 }
             }
         }
